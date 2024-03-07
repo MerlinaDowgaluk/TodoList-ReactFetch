172a84ba62adb651ac14238a9ed87aaf1376c6c5
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2024-03-07 09:11:29 +0000

    styles todolist

--- src/js/component/TodoList-Users.jsx
+++ src/js/component/TodoList-Users.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import Fondo from "../../img/10217044.jpg";
 
 // 1º hay que crear el usuario
 // 2º agregar tareas
@@ -6,11 +7,28 @@ import React, {useEffect, useState} from "react";
 // 4º eliminar usuario
 
 export const TodoListUsers = () =>{
-    const user = 'Merlina'
+    const [ user, setUser] = useState("");
     const [ task, setTask] = useState("");
     const [ list, setList ] = useState([]);
+    const [ login, setLogin ] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+    const [ errorLogin, setErrorLogin ] = useState(false);
+
 
     const baseUrl = 'https://playground.4geeks.com/apis/fake/todos';
+
+    const handleOnChange = () => {
+		setIsChecked(!isChecked);
+	};
+
+    const handleOnSubmit = (event) =>{
+        event.preventDefault();
+        if (isChecked == false){
+            getTodo();
+        } else {
+            createUser();
+        }
+    }
 
     const createUser = async () =>{
         const url = baseUrl + '/user/' + user;
@@ -25,8 +43,12 @@ export const TodoListUsers = () =>{
         if (response.ok){
             const data = await response.json();
             console.log(data);
+            setLogin(true);
+            getTodo()
 
         } else {
+            setLogin(false);
+            setErrorLogin(true);
             return ('Error: ', response.status, response.statusText)
         }
     }
@@ -40,10 +62,13 @@ export const TodoListUsers = () =>{
         if (response.ok){
             const data = await response.json();
             console.log(data);
-            setList(data)
+            setList(data);
+            setLogin(true);
 
         } else {
-            return ('Error: ', response.status, response.statusText)
+            setLogin(false);
+            setErrorLogin(true);
+            return ('Error: ', response.status, response.statusText);
         }
     }
 
@@ -60,8 +85,6 @@ export const TodoListUsers = () =>{
         if (response.ok){
             const data = await response.json();
             console.log(data);
-            
-
         } else {
             return ('Error: ', response.status, response.statusText)
         }
@@ -78,6 +101,17 @@ export const TodoListUsers = () =>{
         setTask("")
     }
 
+    const taskDone = (item) =>{
+        const newTaskDone = {label: item.label, done: true};
+        const listWithoutDelete = list.filter((element, id) =>{
+            if (item.id != id){
+                return item != element;
+            }
+        });
+        setList([...listWithoutDelete, newTaskDone]);
+        actualiceTaskwithDelete(list);
+    }
+
     const deleteUser = async () =>{
         const url = baseUrl + '/user/' + user;
         const options = {
@@ -87,8 +121,9 @@ export const TodoListUsers = () =>{
         if (response.ok){
             const data = await response.json();
             console.log(data);
-            setList([])
-
+            setList([]);
+            setLogin(false);
+            setErrorLogin(false)
         } else {
             return ('Error: ', response.status, response.statusText)
         }
@@ -96,7 +131,9 @@ export const TodoListUsers = () =>{
 
     const deleteTask= (item) => {
         const listWithoutDelete = list.filter((element, id) =>{
-            return item != element;
+            if (item.id != id){
+                return item != element;
+            }
         });
         console.log(listWithoutDelete);
         setList(listWithoutDelete);
@@ -116,7 +153,6 @@ export const TodoListUsers = () =>{
         if (response.ok){
             const data = await response.json();
             console.log(data);
-            
 
         } else {
             return ('Error: ', response.status, response.statusText)
@@ -124,47 +160,74 @@ export const TodoListUsers = () =>{
     }
 
 
-    useEffect(() =>{
-        getTodo()
-    }, [])
+    // useEffect(() =>{
+    //     getTodo()
+    // }, [])
 
     return(
         <div className="container">
-            <h1 className="text-center m-2">ToDo List</h1>
-            <button type="button" className="btn btn-success m-2" onClick={createUser} data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Crear nuevo usuario
+            <h1 className="text-center m-2 mt-5">ToDo List</h1>
+            {login == false ?
+            <div>
+            <div className="m-3 justify-content-center">
+            <div className="container col-6 card">
+            <form onSubmit={handleOnSubmit}>
+            <input type="text" className="form-control mt-3 mb-2" value={user} onChange={(e) => setUser(e.target.value)} placeholder="Introduce tu nombre"></input>
+            <div className="form-check text-start">
+				<input type="checkbox" className="form-check-input" id="exampleCheck1" checked={isChecked} onChange={handleOnChange}></input>
+				<label className="form-check-label" htmlFor="exampleCheck1">¿Nuevo usuario?</label>
+			</div>
+            <button type="submit" className="btn btn-success mt-2 mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Guardar
             </button>
-            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-header bg-light rounded">
-                        <h3 className="modal-title fs-5">The user has been created successfully!</h3>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                </div>
+            </form>
             </div>
-            <button type="button" className="btn btn-warning m-2" onClick={getTodo}> 
+            </div>
+            {errorLogin == true ?
+            <div className="alert alert-warning d-block text-center" role="alert">
+                Por favor, revisa los datos. Si es nuevo usuario, tienes que darle a check. Si eres usuario existente, no debes darle click.
+            </div>
+            :
+            <div></div>
+            }
+            </div>
+            :
+            <div className="justify-content-center d-flex m-3">
+            <h3 className="bg-light m-2 col-6 text-center border border-3 rounded-pill pt-3 pb-3">
+                Usuario: {user}
+            </h3>
+            </div>
+            }
+            <div className="justify-content-center d-flex m-3">
+            <button type="button" className="btn btn-warning m-2 col-2" onClick={getTodo}> 
                 Obtener ToDos
             </button>
-            <button type="button" className="btn btn-danger m-2" onClick={deleteUser}>
+            <button type="button" className="btn btn-danger m-2 col-2" onClick={deleteUser}>
                 Eliminar usuario
             </button>
-            <div className="mb-3">
+            </div>
+            <div className="container col-6">
+            <div className="mb-3 justify-content-center">
                 <form onSubmit={addTask}>
                     <input className="form-control" placeholder="What do you need to do?" type="tet" value={task} onChange={(e) =>{setTask(e.target.value);}}/>
                 </form>
             </div>
-            <div className="list">
+            <div className="list justify-content-center">
                 <ul className="list-group"> 
                     {list.map((item, index) => {
                         return <li key={index} className="list-group-item d-flex justify-content-between hidden-icon">
                             {item.label}: {item.done ? 'Terminado' : 'Pendiente'}
-                            <span key={index} onClick={() => {deleteTask(item)}}><i className="far fa-trash-alt text-danger"></i></span>
+                            <div className="justify-content-end">
+                            <span onClick={() => {taskDone(item)}} className={item.done ? "disabled" : " "} aria-disabled={item.done ? "true" : "false"}><i className={item.done ? "disabled" : "fas fa-solid fa-check m-1 text-success"}></i></span>
+                            <span key={index} onClick={() => {deleteTask(item)}}><i className="far fa-trash-alt text-danger m-1"></i></span>
+                            </div>
                         </li>
                     })}
                     <span className="list-group-item bg-light text-end fw-lighter">
-                        {list.length === 0 ? "No tasks, add one please" : list.length + " items left."}
+                        {list.length === 0 ? "No tasks, add one please" : list.length + " tareas."}
                     </span>
                 </ul>
+            </div>
             </div>
         </div>
     )
